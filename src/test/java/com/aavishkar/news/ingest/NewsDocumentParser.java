657ffcce67479a5e945bafddb7fa799dfaa1f3a3
Anish Somani <asomani0@gmail.com>
2016-06-19 11:59:49 +0530

    Sending document to elasticsearch , optimized code

--- src/test/java/com/aavishkar/news/ingest/NewsDocumentParser.java
+++ src/test/java/com/aavishkar/news/ingest/NewsDocumentParser.java
@@ -1,7 +1,11 @@
 package com.aavishkar.news.ingest;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -11,6 +15,8 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
+
+import com.google.gson.Gson;
 
 public class NewsDocumentParser {
 	public static void main(String[] args) {
@@ -30,35 +36,39 @@ public class NewsDocumentParser {
 			List<Element> rowList = docElement.getChildren();
 			System.out.println("----------------------------");
 
-			for (Element row: rowList) {
-				readAndWriteToMap(row, map, new String[]{"APPLICATION_ID", "IC_NAME", "ORG_CITY", "ORG_DEPT",
-						"ORG_COUNTRY", "ORG_NAME", "PHR" , 
-				});
+			for (Element row : rowList) {
+				readAndWriteToMap(row, map, new String[] { "APPLICATION_ID", "IC_NAME", "ORG_CITY", "ORG_DEPT",
+						"ORG_COUNTRY", "ORG_NAME", "ORG_STATE", "PHR", });
 				List<Element> list = row.getChild("PIS").getChildren("PI");
-				
+
 				StringBuilder sb = new StringBuilder();
 				boolean first = true;
-				for (Element elem: list) {
+				for (Element elem : list) {
 					if (!first) {
 						sb.append(";");
 					}
 					first = false;
 					String childText1 = elem.getChildText("PI_NAME");
-					childText1 = capitalize(childText1);
-					
-					sb.append(childText1);
+
+					sb.append(capitalize(childText1));
 				}
-				
-				map.put("PI_NAME", sb.toString());
-                readAndWriteToMap(row, map, new String[]{"PROGRAM_OFFICER_NAME","PROJECT_START","PROJECT_END",
-                		"PROJECT_TITLE","FUNDING_MECHANISM","STUDY_SECTION_NAME"});
-                
-                List<String> list2 = new ArrayList<String>();
-				for (Element elem: row.getChild("PROJECT_TERMSX").getChildren("TERM")) {
-					list2.add("\""+capitalize(elem.getText())+"\"");
+
+				map.put(capitalize("PI_NAME"), sb.toString());
+
+				readAndWriteToMap(row, map, new String[] { "PROGRAM_OFFICER_NAME", "PROJECT_START", "PROJECT_END", });
+
+				List<String> list2 = new ArrayList<String>();
+				for (Element elem : row.getChild("PROJECT_TERMSX").getChildren("TERM")) {
+					list2.add("\"" + capitalize(elem.getText()) + "\"");
 				}
-				map.put("PROJECT_TERMSX", list2.toString());
-				System.out.println(map);
+				map.put(capitalize("PROJECT_TERMSX"), list2.toString());
+
+				readAndWriteToMap(row, map,
+						new String[] { "PROJECT_TITLE", "FUNDING_MECHANISM", "STUDY_SECTION_NAME" });
+
+				storeToElasticSearch("data", "news", map);
+
+				// System.out.println(map);
 
 				map.clear();
 			}
@@ -68,33 +78,67 @@ public class NewsDocumentParser {
 			ioe.printStackTrace();
 		}
 	}
-	
-	public static void readAndWriteToMap(Element row, Map<String, String> map, String ... keys) {
-		for (String key: keys) {
-			String childText = row.getChildText(key);
-			childText = capitalize(childText);
 
-			map.put(key, childText);
+	private static void storeToElasticSearch(String index, String type, Map<String, String> data) throws IOException {
+		String hostPort = "http://localhost:9200";
+		String applicationId = data.get("Application_Id");
+		if (applicationId == null) {
+			System.out.println("Application id missing for " + data);
+			return;
+		}
+
+		URL url = new URL(hostPort + "/" + index + "/" + type + "/" + applicationId);
+		
+		//Delete existing document
+		HttpURLConnection httpCon1 = (HttpURLConnection) url.openConnection();
+		httpCon1.setDoOutput(true);
+		httpCon1.setRequestMethod("DELETE");
+		OutputStreamWriter out1 = new OutputStreamWriter(httpCon1.getOutputStream());
+		out1.close();
+		try {
+			httpCon1.getInputStream();
+		} catch (FileNotFoundException e) {
+			//Ignore, document doesn't exist
+		}
+		
+		//Insert new document
+		HttpURLConnection httpCon2 = (HttpURLConnection) url.openConnection();
+		httpCon2.setDoOutput(true);
+		httpCon2.setRequestMethod("PUT");
+		OutputStreamWriter out2 = new OutputStreamWriter(httpCon2.getOutputStream());
+		Gson gson = new Gson();
+		String string = gson.toJson(data);
+		out2.write(string);
+		out2.close();
+		httpCon2.getInputStream();
+
+		System.out.println("Completed ingest of application id: " + applicationId);
+	}
+
+	public static void readAndWriteToMap(Element row, Map<String, String> map, String... keys) {
+		for (String key : keys) {
+			String childText = row.getChildText(key);
+			map.put(capitalize(key), capitalize(childText));
 		}
 	}
 
-	
 	public static String capitalize(String name) {
 		if (name == null) {
 			return null;
 		}
-		
+
 		name = name.toLowerCase();
 		StringBuilder name1 = new StringBuilder();
-		for (int x = 0 ; x < name.length(); x++)
-		{
-			if (x == 0 || name.charAt(x-1) == ' ' || name.charAt(x-1) == '\t' || name.charAt(x-1) == '\n'){
-                 name1.append(Character.toUpperCase(name.charAt(x)));		
+		for (int x = 0; x < name.length(); x++) {
+			if (x == 0 || name.charAt(x - 1) == ' ' || name.charAt(x - 1) == '\t' || name.charAt(x - 1) == '\n'
+					|| name.charAt(x - 1) == '_') {
+				name1.append(Character.toUpperCase(name.charAt(x)));
+			} else {
+				name1.append(name.charAt(x));
 			}
-			else {
-			     name1.append(name.charAt (x));	
-			};
-		};
+			;
+		}
+		;
 		return name1.toString();
 	}
 }
--- src/test/java/com/aavishkar/news/ingest/ParseAndStringTest.java
+++ src/test/java/com/aavishkar/news/ingest/ParseAndStringTest.java
@@ -2,25 +2,44 @@ package com.aavishkar.news.ingest;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+
+import com.google.gson.Gson;
 
 import junit.framework.TestCase;
 
 public class ParseAndStringTest extends TestCase {
 	public void testMap() {
-		Map<String,String> map = new HashMap<String, String>();
+		Map<String, String> map = new HashMap<String, String>();
 		map.put("student1", "Anish");
 		map.put("student2", "Vivek");
 		map.put("studnet3", "Priyal");
 		map.put("student1", "Anusha");
 		assertEquals(3, map.size());
 	}
-	
+
 	public void testCapitalize() {
 		String name = "AKHLAGHI, FATEMEH\nand\tok";
 		String expected = "Akhlaghi, Fatemeh\nAnd\tOk";
-		 name = NewsDocumentParser.capitalize(name);
-		
-		assertEquals(expected, name);
+		assertEquals(expected, NewsDocumentParser.capitalize(name));
+
+		name = "AKHLAGHI_FATEMEH\nand\tok";
+		expected = "Akhlaghi_Fatemeh\nAnd\tOk";
+
+		assertEquals(expected, NewsDocumentParser.capitalize(name));
+	}
+
+	public void testJsonFromMap() {
+		Map<String, String> map = new TreeMap<String, String>();
+		map.put("student1", "Anish");
+		map.put("student2", "Vivek");
+		map.put("student3", "Priyal");
+		map.put("student1", "Anusha");
+		Gson gson = new Gson();
+		String string = gson.toJson(map);
+		assertTrue(string.contains("Anusha"));
+		assertTrue(string.contains("Vivek"));
+		assertFalse(string.contains("Anish"));
 	}
 
 }
